8c37f8f091695c5a949b9ebb29d0b9c041da1963
SuryaShakti <suryashakti1999@gmail.com>
2021-08-05 23:44:03 +0530

    Botom Container added || SIde options added

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,16 +1,32 @@
 import React, { useState } from 'react'
-import { View, Text, TouchableWithoutFeedback } from 'react-native'
+import { View, Text, TouchableWithoutFeedback, Image, TouchableOpacity } from 'react-native'
 import Video from 'react-native-video'
 import styles from './styles'
+import Entypo from 'react-native-vector-icons/Entypo'
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Fontisto from 'react-native-vector-icons/Fontisto';
 
-const Post = () => {
 
+const Post = (props) => {
+    const [post, setPost] = useState(props.post);
     const [paused, setPaused] = useState(false);
+    const [isLiked, setIsLiked] = useState(false);
+
 
     const onPlayPausePress = () => {
         // console.warn('pauseeeeeee')
         setPaused(!paused);
     }
+
+    const onLikePress = () => {
+        const likesToAdd = isLiked ? -1 : 1;
+        setPost({
+            ...post,
+            likes: post.likes + likesToAdd,
+        });
+        setIsLiked(!isLiked);
+    };
 
     return (
         <View style={styles.container}>
@@ -25,7 +41,52 @@ const Post = () => {
                     paused={paused}
                 />
             </TouchableWithoutFeedback>
-        </View>
+
+            <View style={styles.uiContainer}>
+
+                <View style={styles.rightContainer}>
+                    <Image
+                        style={styles.profilePicture}
+                        source={{ uri: 'https://miro.medium.com/fit/c/1360/1360/1*I-rnOsWRz_5WnlXCpV9jJQ.jpeg' }}
+                    />
+
+                    <TouchableOpacity style={styles.iconContainer} onPress={onLikePress}>
+                        <AntDesign name={'heart'} size={40} color={isLiked ? 'red' : 'white'} />
+                        <Text style={styles.statsLabel}>{'245'}</Text>
+                    </TouchableOpacity>
+
+                    <View style={styles.iconContainer}>
+                        <FontAwesome name={'commenting'} size={40} color="white" />
+                        <Text style={styles.statsLabel}>{'123'}</Text>
+                    </View>
+
+                    <View style={styles.iconContainer}>
+                        <Fontisto name={'share-a'} size={35} color="white" />
+                        <Text style={styles.statsLabel}>{12}</Text>
+                    </View>
+                </View>
+
+                <View style={styles.bottomContainer}>
+                    <View>
+                        <Text style={styles.handle}>
+                            {'@SuryaShakti'}
+                        </Text>
+                        <Text style={styles.description} >
+                            {'Description of the post will be dislayed here'}
+                        </Text>
+                        <View style={styles.songContainer}>
+                            <Entypo style={styles.songIcon} name={'beamed-note'} size={24} color={'white'} />
+                            <Text style={styles.songName}>{'Nf - The Search'}</Text>
+                        </View>
+                    </View>
+
+                    {/* <Image
+                        style={styles.songImage}
+                        source={{ uri: post.song.imageUri }}
+                    /> */}
+                </View>
+            </View>
+        </View >
     )
 }
 
--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -1,9 +1,9 @@
-import { StyleSheet, Dimensions } from "react-native";
+import { StyleSheet, Dimensions, StatusBar } from "react-native";
 
 const styles = StyleSheet.create({
     container: {
         width: '100%',
-        height: Dimensions.get('window').height
+        height: Dimensions.get('window').height - StatusBar.currentHeight
     },
     video: {
         position: 'absolute',
@@ -11,7 +11,68 @@ const styles = StyleSheet.create({
         left: 0,
         right: 0,
         bottom: 0,
-    }
+    },
+    uiContainer: {
+        height: '100%',
+        justifyContent: 'flex-end',
+    },
+    bottomContainer: {
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+    },
+    handle: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: '700',
+        marginBottom: 10,
+    },
+    description: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: '300',
+        marginBottom: 10,
+    },
+    songContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    songImage: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        borderWidth: 5,
+        borderColor: '#4c4c4c',
+    },
+    songName: {
+        color: '#fff',
+        fontSize: 16,
+        marginLeft: 5,
+    },
+    rightContainer: {
+        alignSelf: 'flex-end',
+        height: 300,
+        justifyContent: 'space-between',
+        marginRight: 5,
+    },
+    profilePicture: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        borderWidth: 2,
+        borderColor: '#fff',
+    },
+
+    iconContainer: {
+        alignItems: 'center',
+    },
+    statsLabel: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: '600',
+        marginTop: 5,
+    },
 })
 
 export default styles;
