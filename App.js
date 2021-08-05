8f1a58ba0d56ee35f29b3c68a55e5f81725071f4
SuryaShakti <suryashakti1999@gmail.com>
2021-08-05 18:37:03 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type { Node } from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import HomeScreen from './src/screens/Home';
+
+const App: () => Node = () => {
+  return (
+    <>
+      <StatusBar barStyle={'dark-content'} />
+      <SafeAreaView>
+        <HomeScreen />
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
